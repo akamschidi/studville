e157c500f6621dfed1826c2a927d902e3ead6f6e
Akamz Chidi <akamschidi@gmail.com>
2025-07-29 15:31:35 +0100

    third update CRUD complete

--- src/main/java/com/studville/studville/Controller/StudController.java
+++ src/main/java/com/studville/studville/Controller/StudController.java
@@ -3,7 +3,6 @@ package com.studville.studville.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studville.studville.Entity.Stud;
-import com.studville.studville.Entity.Stud;
 import com.studville.studville.StudService.StudService;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +11,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -33,16 +37,42 @@ public class StudController {
         Stud saveStud = service.addStud(stud);
         return new ResponseEntity<>(saveStud, HttpStatus.CREATED);
         }
-
+/* 
     @GetMapping("/allstuds")
     public List<Stud> allStuds() {
         return service.getAllStuds();
     }
+*/
+//use pageable to split the list of all data into smaller chunks of data to optimize and increase speed. 
+    @GetMapping("/allstuds")
+    public ResponseEntity<Page<Stud>> listAllStuds(Pageable pageable){
+        Page<Stud> allStuds = service.listAllstuds(pageable);
+        return ResponseEntity.ok(allStuds);
+    }
 
     @GetMapping("/stud/{id}")
     public ResponseEntity<Stud> findStudById(@PathVariable Long id) {
-        Optional<Stud> studByID = service.findStudByID(id)
-        return new ResponseEntity<>(studByID, HttpStatus.OK);
+       Stud studByID = service.findStudById(id);
+        return  ResponseEntity.ok(studByID);
+    }
+    
+    
+     @GetMapping("/studs/{email}")
+    public ResponseEntity<Stud> findStudByEmail(@PathVariable String email) {
+       Stud studByEmail = service.findStudByEmail(email);
+        return  ResponseEntity.ok(studByEmail);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Stud> updateStud(@PathVariable Long id, @RequestBody Stud stud) {
+        Stud studUpdate = service.updateStud(stud, id);
+         return ResponseEntity.ok(studUpdate);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<Void> removeStud(@PathVariable Long id){
+     service.removeStud(id);
+     return ResponseEntity.noContent().build();
     }
     
     
--- src/main/java/com/studville/studville/StudService/ServiceInterface/StudInterface.java
+++ src/main/java/com/studville/studville/StudService/ServiceInterface/StudInterface.java
@@ -3,12 +3,16 @@ package com.studville.studville.StudService.ServiceInterface;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.studville.studville.Entity.Stud;
 
 public interface StudInterface {
     
     Stud addStud(Stud stud);
     List<Stud> getAllStuds();
+    Page<Stud> listAllstuds(Pageable pageable);
     Stud findStudById(Long Id);
     Stud findStudByEmail(String email);
     Stud updateStud(Stud stud, Long id);
